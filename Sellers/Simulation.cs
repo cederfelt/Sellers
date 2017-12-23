7a9d8405679aa57c5f95017096bff90b144417be
ludvig <cederfelt@live.com>
2017-12-23 19:13:19 +0100

    some basic variables

--- Sellers/Simulation.cs
+++ Sellers/Simulation.cs
@@ -7,10 +7,23 @@ namespace Sellers
     public class Simulation
     {
         private bool[,] Board;
+        private long TotalAmount;
+        private SalesPerson[] Sellers;
 
         public Simulation(long width, long height)
         {
             Board = new bool[width, height];
+            TotalAmount = width * height;
+            Sellers = new SalesPerson[TotalAmount];
+        }
+
+        public void RunSimulation()
+        {
+            long nrSellers = 0;
+            while(nrSellers < TotalAmount)
+            {
+
+            }
         }
 
     }
